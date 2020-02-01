50fae4ea98a45973b80c1c180a7498091f9fc6bd
Trevor Adams <trevorwadams@gmail.com>
2020-02-01 11:36:16 -0700

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# xstate-react-table
-Created with CodeSandbox
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./xstate-table/index.css";
+import "./styles.css";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
